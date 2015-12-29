0c675377d829f64c5431544a7455fb3a7b454ca3
Morris Mukiri <morrismukiri@gmail.com>
2015-12-29 18:11:53 +0300

    Added a github accounts viewer, Added support for bower components --angular-- bootstrap,datatables,jquery

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
-(function(){
-	var userModule=angular.module("userModule",[]);
+(function () {
+	var userModule = angular.module("userModule", []);
 	//Dummy users collection
-	var usersCollection=[
-		{name:"Walter White",age:54,gender:"male"},
-		{name:"Olivia Grey",age:23,gender:"female"},
-		{name:"Joe Black",age:36,gender:"male"}
-		] ;
-	userModule.controller('usersCtrl',['$scope',function($scope){
-		$scope.users=usersCollection;
-		
+	var usersCollection = [
+		{ name: "Walter White", age: 54, gender: "male" },
+		{ name: "Olivia Grey", age: 23, gender: "female" },
+		{ name: "Joe Black", age: 36, gender: "male" }
+	];
+	userModule.controller('usersCtrl', ['$scope', function ($scope) {
+		$scope.users = usersCollection;
+
 	}]);
-	
+
+	var githubModule = angular.module("githubModule", ['datatables']);
+	githubModule.controller("githubCtrl", ['$scope', '$http', function ($scope, $http) {
+		var githubUSername = '';
+		var notFoundError = function (dets) {
+			$scope.error = "Could not find the requested data";
+		}
+
+		$scope.search = function (githubUSername) {
+			console.log('searching...');
+			$http.get('https://api.github.com/users/' + githubUSername).
+				then(function (response) {
+					$scope.user = response.data;
+				}, notFoundError);
+			$http.get('https://api.github.com/users/' + githubUSername + '/repos').
+				then(function (response) {
+					$scope.repos = response.data;
+				}, notFoundError);
+		}
+	}]);
 })()
